d072b6be13c802819fabadaf19770a8b041bd9e9
stack72 <public@paulstack.co.uk>
2019-08-28 23:00:57 +0300

    Regenerate SDK based on latest version of Pulumi Terraform

--- sdk/python/pulumi_mysql/database.py
+++ sdk/python/pulumi_mysql/database.py
@@ -6,6 +6,7 @@ import json
 import warnings
 import pulumi
 import pulumi.runtime
+from typing import Union
 from . import utilities, tables
 
 class Database(pulumi.CustomResource):
@@ -85,6 +86,7 @@ class Database(pulumi.CustomResource):
         """
         Get an existing Database resource's state with the given name, id, and optional extra
         properties used to qualify the lookup.
+        
         :param str resource_name: The unique name of the resulting resource.
         :param str id: The unique provider ID of the resource to lookup.
         :param pulumi.ResourceOptions opts: Options for the resource.
@@ -101,7 +103,7 @@ class Database(pulumi.CustomResource):
 
         > This content is derived from https://github.com/terraform-providers/terraform-provider-mysql/blob/master/website/docs/r/database.html.markdown.
         """
-        opts = pulumi.ResourceOptions(id=id) if opts is None else opts.merge(pulumi.ResourceOptions(id=id))
+        opts = pulumi.ResourceOptions.merge(opts, pulumi.ResourceOptions(id=id))
 
         __props__ = dict()
         __props__["default_character_set"] = default_character_set
