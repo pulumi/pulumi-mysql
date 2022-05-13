8cda56b7a1498ea69bd8e78f6ab21d16bb13c88f
Josh Kodroff <jkodroff@users.noreply.github.com>
2022-05-13 11:33:01 -0700

    Expect changes on acceptance test. (#124)

--- examples/examples_nodejs_test.go
+++ examples/examples_nodejs_test.go
@@ -1,4 +1,5 @@
 // Copyright 2016-2017, Pulumi Corporation.  All rights reserved.
+//go:build nodejs || all
 // +build nodejs all
 
 package examples
@@ -14,6 +15,12 @@ func TestAccDatabase(t *testing.T) {
 	test := getJSBaseOptions(t).
 		With(integration.ProgramTestOptions{
 			Dir: path.Join(getCwd(t), "database"),
+			// The default character set for the MySQL Docker image has diverged from the upstream provider's value.
+			// Since the upstream provider has been archived and will not be updated, we'll simply expect changes after
+			// creating a DB with the default options:
+			AllowEmptyPreviewChanges: true,
+			AllowEmptyUpdateChanges:  true,
+			ExpectRefreshChanges:     true,
 		})
 
 	integration.ProgramTest(t, &test)
